7dd3d1794e65a67e532af2caddb91df82a188d0f
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-03-20 10:52:16 -0700

    Add module installations and DSC configuration packaging

--- iac/modules/packageScript.ps1
+++ iac/modules/packageScript.ps1
@@ -1,4 +1,10 @@
 $name = $Env:MODULE_NAME;
+
+# Install required modules
+Install-Module -Name PSDscResources -RequiredVersion "2.12.0.0" -Force -SkipPublisherCheck -AllowClobber;
+Install-Module -Name WebAdministrationDsc -RequiredVersion "4.1.0" -Force -SkipPublisherCheck -AllowClobber;
+Install-Module -Name GuestConfiguration -RequiredVersion "4.5.0" -Force -SkipPublisherCheck -AllowClobber;
+
 # Save DSC Configuration content to file
 $content = $Env:MODULE_CONTENT;
 
@@ -8,10 +14,10 @@ $filePath = "./${name}.ps1"
 # Save the DSC Configuration content to the file
 Set-Content -Path $filePath -Value $content
 
-# Execute the Powershell script
-& $filePath;
+# Import the DSC Module
+Import-Module $filePath;
 
-# Execute the Powershell function
+# Compile the DSC Module
 $params = @{
     siteName = $Env:SITE_NAME;
     applicationPool = $Env:APPLICATION_POOL;
@@ -25,3 +31,13 @@ $params = @{
     ErrorAction = "Stop";
 }
 & $name @params;
+
+# Create the configuration package
+New-GuestConfigurationPackage -Name $name `
+                    -Configuration ".\output\localhost.mof" `
+                    -Type "AuditAndSet" `
+                    -Force $true;
+
+# Upload the DSC configuration to the storage account
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $Env:AZURE_RESOURCE_GROUP -Name $Env:AZURE_STORAGE_ACCOUNT;
+Set-AzStorageBlobContent -File "${name}.zip" -Container $Env:AZURE_STORAGE_CONTAINER -Blob "${name}.zip" -Context $storageAccount.Context -Force;
